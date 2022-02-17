8c5a22c7c4b7baa3042e0f51bbc7d27c96a52eda
ishadoww <2002004@utrivieramaya.edu.mx>
2022-02-17 12:38:17 -0600

    Initial commit

---
+++ app/src/main/java/com/example/activities/Activity2.java
@@ -0,0 +1,22 @@
+package com.example.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+    //Metodo boton anterior
+    public void Anterior(View view){
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
+
+    }
+}
